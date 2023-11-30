3b8a3352ef177ac27f9d95326e8d99a65fbe1408
fmatt04 <fmatt04@vt.edu>
2023-11-29 22:56:29 -0500

    BLOCKS

--- sketch.js
+++ sketch.js
@@ -1,12 +1,16 @@
 let player1;
 let direction;
 let jump;
+let block1;
+let framesPerSecond = 60;
 
 function setup() {
-  createCanvas(800, 500);
+  frameRate(framesPerSecond);
+  createCanvas(700, 300);
   rectMode(CENTER);
   textAlign(CENTER);
   player1 = new player();
+  block1 = new block(100, 275, 100, 50);
 }
 
 ////////draw
@@ -14,13 +18,16 @@ function draw() {
   background(220);
   //function calls
   player1.move();
+  block1.drawBox();
 }
 
 class player {
 
   constructor() {
     this.posX = 200;
+    this.posXPast = 200;
     this.posY = 200;
+    this.posYPast = 200;
     this.speedX = 0;
     this.speedY = 0;
     this.grounded = false;
@@ -31,7 +38,7 @@ class player {
   move() {
     if (keyIsDown(LEFT_ARROW)) {
       if (this.speedX < 0) {
-        this.speedX -= .5;
+        this.speedX -= 0.5;
       }
       else {
         this.speedX -= 1;
@@ -39,7 +46,7 @@ class player {
     }
     else if (keyIsDown(RIGHT_ARROW)) {
       if (this.speedX > 0) {
-        this.speedX += .5;
+        this.speedX += 0.5;
       }
       else {
         this.speedX += 1
@@ -57,10 +64,8 @@ class player {
       }
     }
     if (keyIsDown(UP_ARROW) || keyIsDown(32)) {
-      console.log("1");
       if (this.grounded) {
-        console.log("2");
-        this.speedY = -15;
+        this.speedY = -17;
         this.grounded = false;
       }
     }
@@ -81,12 +86,50 @@ class player {
       this.posX = this.playerWidth/2;
       this.speedX = 0;
     }
+    
+    //block 1 
+    this.blockOverlap(block1);
+    //console.log(this.posY + this.playerHeight/2);
+    /*if (block1.inBlockX(this.posX - this.playerWidth/2, this.posX + this.playerWidth/2)
+    && block1.inBlockY(this.posY + this.playerHeight/2))
+    {
+      console.log("overlap");
+      if (block1.inBlockY(1+this.posY + this.playerHeight/2)) {
+        this.speedY = 0;
+        this.posY = this.posYPast;
+        this.grounded = true;
+      }
+      else if (block1.inBlockX(this.posX - this.playerWidth/2, this.posX + this.playerWidth/2)) {
+        this.speedX = 0; 
+        this.posX = this.posXPast;
+      }
+    }*/
+
     this.gravity();
 
     //draw character
     fill(100, 30, 200);
     stroke(0);
     rect(this.posX, this.posY, this.playerWidth, this.playerHeight);
+    this.posXPast = this.posX;
+    this.posYPast = this.posY;
+  }
+
+  blockOverlap(block) {
+    if (block.inBlockX(this.posX - this.playerWidth/2, this.posX + this.playerWidth/2)
+    && block.inBlockY(this.posY + this.playerHeight/2))
+    {
+      console.log("overlap");
+      if (block.inBlockY(1+this.posY + this.playerHeight/2)) {
+        this.speedY = 0;
+        this.posY = this.posYPast;
+        this.grounded = true;
+      }
+      else if (block.inBlockX(this.posX - this.playerWidth/2, this.posX + this.playerWidth/2)) {
+        this.speedX = 0; 
+        this.posX = this.posXPast;
+      }
+    }
   }
 
   gravity() {
@@ -99,5 +142,39 @@ class player {
     if (!this.grounded) {
       this.speedY += 1;
     }
+  }
+}
+
+class block {
+  constructor(bXPos, bYPos, bWidth, bHeight) {
+    this.boxXPos = bXPos;
+    this.boxYPos = bYPos;
+    this.boxWidth = bWidth;
+    this.boxHeight = bHeight;
+  }
+
+  drawBox () {
+    rect(this.boxXPos, this.boxYPos, this.boxWidth, this.boxHeight);
+  }
+
+  //collission detector for the box's X value
+  inBlockX (cord1, cord2) {
+    if ((cord1 >= this.boxXPos - this.boxWidth/2 
+    && cord1 <= this.boxXPos + this.boxWidth/2 )
+    || (cord2 >= this.boxXPos - this.boxWidth/2 
+    && cord2 <= this.boxXPos + this.boxWidth/2 )
+    ) {
+      return true;
+    }
+    return false;
+  }
+
+  //collission detector for the box's Y value
+  inBlockY (cord) {
+    if (cord <= this.boxYPos + this.boxHeight/2
+    && cord >= this.boxYPos - this.boxHeight/2) {
+      return true;
+    }
+    return false;
   }
 }
